90f507b1df03e0a96af4f371407715a0933ce1a0
nkaffash <najmeh.kaffashzadeh@gmail.com>
2022-08-22 10:29:32 +0430

    EvalPlotOzoneData.py was modified.

--- src/EvalPlotOzoneData.py
+++ src/EvalPlotOzoneData.py
@@ -72,6 +72,10 @@ class Assess:
     def plot_time_series(self, df=None, ax=None):
         """"
         It plots the time series of the dataframe.
+
+        Args:
+           df(pd.DataFrame): data series
+           ax(object): axis
         """
         df.plot(ax=ax)
         plt.xlabel('date-time')
@@ -84,7 +88,7 @@ class Assess:
         Args:
             fn(str): file name
         """
-        plt.savefig('../plots/ts/'+fn+'.png', bbox_inches='tight')
+        plt.savefig('../plots/'+fn+'.png', bbox_inches='tight')
         plt.close()
 
     def plot_ts_spect(self):
@@ -93,37 +97,56 @@ class Assess:
         """
         fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(13., 8.), facecolor='w')
         df = self.df_obs
-        # df[('dfo', 'S')]
         # self.df_obs['ORG'].plot(ax=ax[0,0], c='k', title='ORG') # sharex=ax[1,0],
         for i,j,sp,t in [(0,0,'ORG', '(a)'), (0,1,'S','(b)'), (1,0,'M','(c)'), (1,1,'L','(d)')]:
-            #df.loc[df.columns.get_level_values(1) == sp]['2020-06':'2020-08'].plot(ax=ax[i, j], title=t, legend=False)
+            # df.loc[df.columns.get_level_values(1) == sp]['2020-06':'2020-08'].plot(ax=ax[i, j], title=t, legend=False)
             df[('dfo',sp)]['2020-06':'2020-08'].plot(ax=ax[i,j], title=t,legend=False, c='k')
             df[('dfr', sp)]['2020-06':'2020-08'].plot(ax=ax[i, j], title=t, legend=False, c='red')
             df[('dfa', sp)]['2020-06':'2020-08'].plot(ax=ax[i, j], title=t, legend=False, c='dodgerblue')
-        # plt.subplots_adjust()
         fig.tight_layout()
         plt.savefig('spec.jpg') # bbox_inches='tight')
         plt.close()
-        exit()
 
     def calc_mse(self, df1=None, df2=None):
+        """
+        It calculates the mse.
+
+        Args:
+            df1(pd.DataFrame): the first data series
+            df2(pd.DataFrame): the second data series
+
+        Returns:
+               three mse portions and correlations
+        """
         r = df1.corr(df2)
-        print([((df2 - df1)**2).mean(),
-                r,
-                (df2.std() - (r * df1.std())) ** 2,
-                (df1.std() ** 2) * (1. - r ** 2),])
         return [((df2 - df1)**2).mean(),
                 (df2.std() - (r * df1.std())) ** 2,
                 (df1.std() ** 2) * (1. - r ** 2),
                 r]
 
     def calc_stats(self, df1=None, df2=None):
+        """
+        It calculates the covariance between two series.
+
+        Args:
+            df1(pd.DataFrame): the first data series
+            df2(pd.DataFrame): the second data series
+
+        Returns:
+              correlation, covariance and variances of two series
+        """
         return [df1.corr(df2),
                 self.calc_cov(df1=df1, df2=df2),
                 self.calc_var(df=df1), self.calc_var(df=df2),
                 ]
 
     def plot_box_scatter(self, vals=None):
+        """
+        It plots the values as box.
+
+        Args:
+            vals(list): a list of mse, e1, e2 and e3
+        """
         fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(13,8), sharex=True)
         ax[0].boxplot([[i[0] for i in vals['1']], [i[0] for i in vals['2']],
                        [i[0] for i in vals['3']], [i[0] for i in vals['4']]], showmeans=True, showfliers=True,
@@ -152,6 +175,12 @@ class Assess:
         plt.close()
 
     def plot_box_scatter_stats(self, vals=None):
+        """
+        It plots the values as box.
+
+         Args:
+              vals(list): a list of correlation, covariance and variances.
+        """
         fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(13,8))
         ax[0].boxplot([[i[0] for i in vals['1']], [i[0] for i in vals['2']],
                        [i[0] for i in vals['3']], [i[0] for i in vals['4']]], showmeans=True, showfliers=True,
@@ -189,6 +218,16 @@ class Assess:
         plt.close()
 
     def calc_rel_var(self, df=None, name=None):
+        """
+        It calculates the covariance between two series.
+
+        Args:
+            df(pd.DataFrame): the first data series
+            name(str): station's name
+
+        Returns:
+                relative variances
+        """
         vars=[]
         vt = self.calc_var(df=df[(name, 'ORG')])
         for sp in ['S','M','L']:
@@ -196,16 +235,19 @@ class Assess:
         return vars
 
     def plot_box_scatter_vars(self, vals=None):
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(13,8))
+        """
+        It plots the vars.
+
+        Args:
+            vals(list): values of _
+        """
         plt.boxplot([[i[0] for i in vals], [i[1] for i in vals],
                        [i[2] for i in vals]], showmeans=True, showfliers=True,
                        medianprops=dict(linestyle='-', linewidth=1.5, color='orange'),
                        meanprops=dict(marker='D', markeredgecolor='black', markerfacecolor='firebrick'))
-        # ax[0].set_yscale('log')
         plt.ylabel('(%)',fontsize=14)
         plt.ylim(0, 100)
         plt.show()
-        # ax[0].set_title('(a)')
 
     def run(self):
         """
@@ -216,15 +258,16 @@ class Assess:
         for sta in dfs.columns.levels[0]:
             # sta='8'
             df = dfs[sta]
-            # to plot spect.png
+            # PLOT spec.png
             # self.df_obs = dfs[sta]
             # self.plot_ts_spect()
-            # to plot given time series
+            # PLOT given time series
             # df[(sta, 'dfo', 'ORG')]['2020-06'].plot(c='k')
             # plt.ylabel('O$_{3}$ (nmol mol$^{-1}$)')
             # fig.tight_layout()
             # plt.savefig('4ml.jpg')# bbox_inches='tight')
             # plt.close()
+            # PLOT mse.png
             res['1'].append(self.calc_mse(df1=df[('dfo', 'S')], df2=df[('dfr', 'S')]))
             res['2'].append(self.calc_mse(df1=df[('dfo', 'S')], df2=df[('dfa', 'S')]))
             res['3'].append(self.calc_mse(df1=df[('dfo', 'M')], df2=df[('dfr', 'M')]))
@@ -234,9 +277,8 @@ class Assess:
             # res['2'].append(self.calc_stats(df1=df[('dfo', 'S')], df2=df[('dfa', 'S')]))
             # res['3'].append(self.calc_stats(df1=df[('dfo', 'M')], df2=df[('dfr', 'M')]))
             # res['4'].append(self.calc_stats(df1=df[('dfo', 'M')], df2=df[('dfa', 'M')]))
-        # plt.boxplot([res['1'], res['2'], res['3'], res['4']])
-        self.plot_box_scatter_stats(vals=res)
-        exit()
+        plt.boxplot([res['1'], res['2'], res['3'], res['4']])
+        # self.plot_box_scatter_stats(vals=res)
 
 if __name__ == '__main__':
     Assess().run()
